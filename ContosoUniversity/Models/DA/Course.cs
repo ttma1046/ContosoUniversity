ae085dbea7200c1a62d93fa297e08d76ae47c51f
Chandler Fang <cfang@jadeworld.com>
2018-01-23 18:47:00 +1100

    Project Initial.

---
+++ ContosoUniversity/Models/DA/Course.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ContosoUniversity.Models.DA
+{
+    public class Course
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int CourseId { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
