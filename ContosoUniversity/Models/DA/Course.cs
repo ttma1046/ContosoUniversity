25f688b48e8fb12b1a98cd581830e71bc5900621
Chandler Fang <chandlerfang@gmail.com>
2018-01-23 23:14:33 +1100

    A few changes.

--- ContosoUniversity/Models/DA/Course.cs
+++ ContosoUniversity/Models/DA/Course.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Web;
 
 namespace ContosoUniversity.Models.DA
 {
@@ -12,6 +10,10 @@ namespace ContosoUniversity.Models.DA
         public int CourseId { get; set; }
         public string Title { get; set; }
         public int Credits { get; set; }
+
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
+        public decimal Price { get; set; }
 
         public virtual ICollection<Enrollment> Enrollments { get; set; }
     }
--- ContosoUniversity/Models/DA/Student.cs
+++ ContosoUniversity/Models/DA/Student.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,8 +9,20 @@ namespace ContosoUniversity.Models.DA
     public class Student
     {
         public int StudentId { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z'\s]*$")]
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
         public string LastName { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z'\s]*$")]
+        [Required]
+        [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
         public string FirstMidName { get; set; }
+
+        [Display(Name = "Enrollment Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime EnrollmentDate { get; set; }
 
         public virtual ICollection<Enrollment> Enrollments { get; set; }
